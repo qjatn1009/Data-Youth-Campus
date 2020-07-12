ed1b678bd01b514a65f13b8b61e2009fcf87480c
qjatn1009 <a43727702@gmail.com>
2020-07-12 16:25:25 +0900

    고객 정보관리 시스템 모듈화

--- customer_management/customer_management.py
+++ customer_management/customer_management.py
@@ -10,49 +10,51 @@ from module.print_info import print_menu
 from module.cust_data import saveData
 from module.cust_data import loadData
 
-def main():
-    information = []
-    current, information=loadData(information)
-    while True:
+class customer_management:
+    main(self):
+        information = []
+        current, information=loadData(information)
+        while True:
 
-        menu = print_menu()
+            menu = print_menu()
 
-        if menu == "I":
-            
-            current = input_info(information, current)
-            
-        elif menu == "S":
+            if menu == "I":
+                
+                current = input_info(information, current)
+                
+            elif menu == "S":
 
-            current = search_cust(information, current)
+                current = search_cust(information, current)
 
-        elif menu == "C":
-            
-            current_cust(information, current)
+            elif menu == "C":
+                
+                current_cust(information, current)
 
-        elif menu == "P":
-            
-            current = previous_cust(information, current)
+            elif menu == "P":
+                
+                current = previous_cust(information, current)
 
-        elif menu == "N":
-            
-            current = next_cust(information, current)
+            elif menu == "N":
+                
+                current = next_cust(information, current)
 
-        elif menu == "U":
+            elif menu == "U":
 
-            modify_cust(information, current)
-            
-        elif menu =="A":
-            
-            all_cust(information)
+                modify_cust(information, current)
+                
+            elif menu =="A":
+                
+                all_cust(information)
 
-        elif menu == "D":
+            elif menu == "D":
 
-            current = delete_cust(information, current)
+                current = delete_cust(information, current)
 
-        elif menu == "Q":
-            print("프로그램을 종료합니다.")
-            saveData(information)
-            break
+            elif menu == "Q":
+                print("프로그램을 종료합니다.")
+                saveData(information)
+                break
 
 if __name__ =="__main__":
-    main()
+    customer_manager = customer_management()
+    customer_manager.main()
